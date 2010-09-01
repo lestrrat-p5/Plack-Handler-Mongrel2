abd68818d121c8d9ec9cf2482b915dde90d4ffaa
lestrrat <lestrrat+github@gmail.com>
2010-09-01 21:07:01 +0900

    version++

--- lib/Plack/Handler/Mongrel2.pm
+++ lib/Plack/Handler/Mongrel2.pm
@@ -1,7 +1,7 @@
 package Plack::Handler::Mongrel2;
 use strict;
 use base qw(Plack::Handler);
-our $VERSION = '0.01000_01';
+our $VERSION = '0.01000_02';
 use ZeroMQ qw( ZMQ_UPSTREAM ZMQ_PUB ZMQ_IDENTITY );
 use JSON qw(decode_json);
 use HTTP::Status qw(status_message);
